e332bc6ce459a2bbd7c1598b1e2e4974c14117e8
Ken Irwin <kirwin@wittenberg.edu>
2015-03-23 10:19:52 -0400

    added $one_per_hour_inits array to config

--- config-example.php
+++ config-example.php
@@ -6,4 +6,13 @@ $mysql_password = "";
 
 $default_init   = "";
 $entries_per_page = 100;
+
+/*
+  If an initiative is intended to usually only have one count per hour, 
+  include it in the $one_per_hour_inits array to allow searching on hours
+  with multiple entries.
+  example: $one_per_hour_inits = array(1,4);
+*/
+
+$one_per_hour_inits = array();
 ?>
