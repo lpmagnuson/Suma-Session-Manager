46539d596b0040e5c53341300c49f14285a9521e
Ken Irwin <kirwin@wittenberg.edu>
2015-05-01 14:37:12 -0400

    add.php demos andwhere.class.php

--- and.php
+++ and.php
@@ -1,46 +1,24 @@
 <?
 header ("Content-type: text/plain");
 include("config.php");
-include("class.php");
+require("andwhere.class.php");
 
 //DoQuery("and occurrence like '2015-05-01%'");
 
 try {
     $awp = new AndWhere();
-    $awp->addCondition ('occurrence','2015-04-20%','LIKE');
+    $awp->AddCondition ('occurrence','2015-04-20%','LIKE');
     $db = ConnectPDO();
-    $q = 'SELECT * FROM count WHERE '.$awp->field.' '.$awp->operator.' '. $awp->placeholder;
+    $q = 'SELECT * FROM count WHERE 1 '.$awp->AndWhereString();
     $stmt = $db->prepare($q);
-    
-    $stmt->bindParam($awp->placeholder,$awp->value,$awp->param_type);
+    $stmt = $awp->Bind($stmt);
     $stmt->execute();
     print_r ($stmt->fetchAll());
+
 } catch (PDOException $e) {
     CatchPDO($e);
   }
 
-
-/*
-
-
-function DoQuery($and_where="") {
-    if (isset($and_where) and ($and_where != "")) {
-        print "<li>Got an 'and_where': $and_where </li>";
-        $and_where_injector = $and_where;
-    }
-    else {
-        $and_where_injector = '';
-    }
-    try {
-        $db = ConnectPDO();
-        $stmt = $db->query("SELECT * FROM count WHERE 1 $and_where_injector order by occurrence DESC LIMIT 0,10");
-        print_r($stmt->fetchAll());
-    }
-    catch (PDOException $e)  {
-        CatchPDO($e);
-    }
-}
-*/
 
 function CatchPDO ($e) {
         echo "An Error occured!"; //user friendly message
